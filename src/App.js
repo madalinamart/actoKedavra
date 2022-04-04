acc1c0d881d3ffb76c1e726612663caa37633827
madalinamart <91364600+madalinamart@users.noreply.github.com>
2022-04-04 18:40:00 +0300

    Moved actor object back to App and passed it to Actor as a prop

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import Actor from './components/Actor'
 
 function App() {
+  const actor = {
+    name: 'Leonardo Dicaprio',
+    score: 10,
+    hobbies: 'Music and dancing naked in the rain',
+    description: 'He is a good guy with a thick mustache',
+  };
 
   return (
     <div className="App">
-   <Actor />
+   <Actor actor={actor} />
     </div>
   );
 }
--- src/components/Actor.js
+++ src/components/Actor.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
-const Actor = () => {
-  const actor = {
-    name: 'Leonardo Dicaprio',
-    score: 10,
-    hobbies: 'Music and dancing naked in the rain',
-    description: 'He is a good guy with a thick mustache',
-  };
+const Actor = ({actor}) => {
 
   return (
     <div>
